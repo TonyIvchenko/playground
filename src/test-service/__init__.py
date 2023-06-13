6d68af7036af2c410cca0e237e5c970728d21bff
Tony Ivchenko <toxa.ivchenko@gmail.com>
2023-06-13 13:58:55 -0800

    Move the service loop out of main.py so it's actually testable

---
+++ src/test-service/__init__.py
@@ -0,0 +1,3 @@
+from .service import build_client, run_forever, run_once
+
+__all__ = ["build_client", "run_forever", "run_once"]
--- src/test-service/main.py
+++ src/test-service/main.py
@@ -1,27 +1,11 @@
-import time
-import redis
+from service import build_client, run_forever
 
 mount_path = "/data"
 
-if __name__ == '__main__':
-    
-    print(f"Hi Bob")
-
-    cache = redis.Redis("redis-service") # by name of container
-
-    while True:
-
-        try:
-            # test redis
-            cache.set("key", "value")
-
-            # sleep for an minute
-            time.sleep(60)
-        
-        except Exception as ex:
-            print(f"Error: {ex}")
-            time.sleep(60)
+if __name__ == "__main__":
+    print("starting test-service")
+    cache = build_client()
+    run_forever(cache)
 
         
             
-
