31e73df559a7f5e725fea74d2359889e264c91cd
Tony Ivchenko <toxa.ivchenko@gmail.com>
2023-06-22 12:02:21 -0800

    Add graceful shutdown handling for SIGINT and SIGTERM

--- src/test-service/main.py
+++ src/test-service/main.py
@@ -1,3 +1,6 @@
+import signal
+import threading
+
 from service import build_client, run_forever
 from settings import load_settings
 
@@ -6,13 +9,29 @@ mount_path = "/data"
 if __name__ == "__main__":
     print("starting test-service")
     settings = load_settings()
+    stop_event = threading.Event()
+
+    def _handle_signal(signum, _frame):
+        print(f"received signal {signum}, stopping service")
+        stop_event.set()
+
+    signal.signal(signal.SIGINT, _handle_signal)
+    signal.signal(signal.SIGTERM, _handle_signal)
+
     cache = build_client(host=settings.redis_host, port=settings.redis_port)
-    run_forever(
-        cache,
-        sleep_seconds=settings.sleep_seconds,
-        key=settings.redis_key,
-        value=settings.redis_value,
-    )
+    try:
+        run_forever(
+            cache,
+            sleep_seconds=settings.sleep_seconds,
+            key=settings.redis_key,
+            value=settings.redis_value,
+            should_stop=stop_event.is_set,
+        )
+    finally:
+        close_method = getattr(cache, "close", None)
+        if callable(close_method):
+            close_method()
+        print("service stopped")
 
         
             
