adf984dd9ac755ed1db49e119fe21889b528a79f
Tony Ivchenko <toxa.ivchenko@gmail.com>
2023-07-03 19:09:20 -0800

    Fix Python package metadata and drop deprecated setup options

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="private",
-    version="1.0.0",
-    package_dir = {"": "src"},
-    packages=find_packages(),
-    install_requires=[
-        ""
-    ],
-    setup_requires=['pytest-runner', 'flake8'],
-    tests_require=['pytest'],
+    name="playground",
+    version="0.1.0",
+    description="Playground services and utilities",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=["redis>=4.5.4"],
+    extras_require={"dev": ["pytest>=7.0"]},
+    python_requires=">=3.8",
 )
