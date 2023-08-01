180ab908bc1741130bd99db349adbb328d542835
Tony Ivchenko <toxa.ivchenko@gmail.com>
2023-08-01 10:25:47 -0800

    Simplify hurricane service to single main module and lean tests

---
+++ tests/hurricane/test_main.py
@@ -0,0 +1,44 @@
+from fastapi.testclient import TestClient
+import pytest
+
+from hurricane_service.main import create_app, load_settings
+
+
+def test_load_settings_defaults_and_port_fallback():
+    defaults = load_settings({})
+    assert defaults.api_port == 8000
+
+    from_port = load_settings({"PORT": "7860"})
+    assert from_port.api_port == 7860
+
+
+def test_load_settings_invalid_port_raises():
+    with pytest.raises(ValueError):
+        load_settings({"API_PORT": "bad"})
+
+
+def test_hurricane_service_endpoints():
+    settings = load_settings({"SERVICE_VERSION": "test-v1", "UI_PATH": "/ui"})
+    app = create_app(settings)
+
+    with TestClient(app) as client:
+        health = client.get("/health")
+        assert health.status_code == 200
+        assert health.json()["status"] == "ok"
+        assert health.json()["model_version"] == "test-v1"
+
+        metadata = client.get("/service-metadata")
+        assert metadata.status_code == 200
+        meta = metadata.json()
+        assert meta["service_id"] == "hurricane-intensity-risk"
+        assert meta["predict_endpoint"] == "/predict"
+
+        prediction = client.post(
+            "/predict",
+            json={"storm_id": "AL09", "vmax_kt": 70, "shear_kt": 10, "sst_c": 29.2},
+        )
+        assert prediction.status_code == 200
+        body = prediction.json()
+        assert body["storm_id"] == "AL09"
+        assert 0.0 <= body["ri_probability_24h"] <= 1.0
+        assert body["risk_level"] in {"low", "moderate", "high", "extreme"}
