b6645442622e12fed3f11c5f07e77639ffc24c71
Tony Ivchenko <toxa.ivchenko@gmail.com>
2023-09-13 11:39:07 -0800

    Add real-data wildfire training pipeline and modeling notebook

--- src/wildfire/README.md
+++ src/wildfire/README.md
@@ -1,13 +1,32 @@
 # Wildfire Service
 
-Minimal standalone Gradio app backed by a trained PyTorch model.
+Standalone Gradio app backed by a PyTorch model trained on real wildfire records.
 
-## Local Run
+## Data + Modeling Workflow
 
 From repo root:
 
 ```bash
-PYTHONPATH=src python3 src/wildfire/scripts/train_model.py
+PYTHONPATH=src python3 src/wildfire/scripts/download_data.py
+```
+
+Explore and evaluate in notebook:
+
+- `src/wildfire/notebooks/wildfire_modeling.ipynb`
+
+Train from script:
+
+```bash
+PYTHONPATH=src python3 src/wildfire/scripts/train_model.py --model-version 0.2.0
+```
+
+The trained artifact is loaded by the app from:
+
+- `src/wildfire/model/wildfire_model.pt`
+
+## Local Run
+
+```bash
 PYTHONPATH=src API_PORT=8010 python3 -m wildfire.main
 ```
 
--- src/wildfire/scripts/train_model.py
+++ src/wildfire/scripts/train_model.py
@@ -1,18 +1,18 @@
-"""Train a small PyTorch model for wildfire ignition risk."""
+"""Train wildfire ignition PyTorch model from real wildfire records."""
 
 from __future__ import annotations
 
 import argparse
-import csv
 from pathlib import Path
-import random
 
+import pandas as pd
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
 
 
 FEATURE_NAMES = ["temp_c", "humidity_pct", "wind_kph", "drought_index"]
+TARGET_NAME = "target"
 
 
 class WildfireMLP(nn.Module):
@@ -30,68 +30,54 @@ class WildfireMLP(nn.Module):
         return self.net(x)
 
 
-def _synthetic_probability(temp_c: float, humidity_pct: float, wind_kph: float, drought_index: float) -> float:
-    signal = (
-        0.10 * (temp_c - 25.0)
-        - 0.06 * (humidity_pct - 30.0)
-        + 0.09 * (wind_kph - 20.0)
-        + 2.2 * (drought_index - 0.5)
-        - 1.6
+def load_raw_dataset(path: Path) -> pd.DataFrame:
+    if not path.exists():
+        raise FileNotFoundError(
+            f"Dataset not found at {path}. Run: `python src/wildfire/scripts/download_data.py`"
+        )
+
+    df = pd.read_csv(path)
+    required = {"temp", "RH", "wind", "DC", "area"}
+    missing = sorted(required - set(df.columns))
+    if missing:
+        raise ValueError(f"Raw dataset is missing expected columns: {missing}")
+    return df
+
+
+def prepare_training_dataframe(raw_df: pd.DataFrame) -> pd.DataFrame:
+    drought = raw_df["DC"].astype(float)
+    drought_min = drought.min()
+    drought_max = drought.max()
+    denom = max(drought_max - drought_min, 1e-6)
+    drought_index = (drought - drought_min) / denom
+
+    training_df = pd.DataFrame(
+        {
+            "temp_c": raw_df["temp"].astype(float),
+            "humidity_pct": raw_df["RH"].astype(float),
+            "wind_kph": raw_df["wind"].astype(float),
+            "drought_index": drought_index.astype(float).clip(0.0, 1.0),
+            TARGET_NAME: (raw_df["area"].astype(float) > 0.0).astype(float),
+        }
     )
-    return float(torch.sigmoid(torch.tensor(signal)).item())
+    training_df = training_df.dropna().reset_index(drop=True)
+    return training_df
 
 
-def generate_synthetic_dataset(samples: int, seed: int) -> tuple[torch.Tensor, torch.Tensor]:
-    rng = random.Random(seed)
-    features: list[list[float]] = []
-    targets: list[float] = []
+def split_dataset(
+    training_df: pd.DataFrame,
+    seed: int,
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    data = training_df.sample(frac=1.0, random_state=seed).reset_index(drop=True)
+    split = int(len(data) * 0.8)
+    train_df = data.iloc[:split]
+    val_df = data.iloc[split:]
 
-    for _ in range(samples):
-        temp_c = rng.uniform(10.0, 45.0)
-        humidity_pct = rng.uniform(5.0, 95.0)
-        wind_kph = rng.uniform(0.0, 80.0)
-        drought_index = rng.uniform(0.0, 1.0)
-
-        base_prob = _synthetic_probability(temp_c, humidity_pct, wind_kph, drought_index)
-        noisy_prob = min(0.99, max(0.01, base_prob + rng.uniform(-0.06, 0.06)))
-        target = 1.0 if rng.random() < noisy_prob else 0.0
-
-        features.append([temp_c, humidity_pct, wind_kph, drought_index])
-        targets.append(target)
-
-    x = torch.tensor(features, dtype=torch.float32)
-    y = torch.tensor(targets, dtype=torch.float32).unsqueeze(1)
-    return x, y
-
-
-def load_csv_dataset(path: Path) -> tuple[torch.Tensor, torch.Tensor]:
-    rows: list[list[float]] = []
-    labels: list[float] = []
-
-    with path.open("r", newline="") as handle:
-        reader = csv.DictReader(handle)
-        required = set(FEATURE_NAMES + ["target"])
-        if reader.fieldnames is None or not required.issubset(set(reader.fieldnames)):
-            raise ValueError(f"CSV must contain headers: {sorted(required)}")
-
-        for row in reader:
-            rows.append([float(row[name]) for name in FEATURE_NAMES])
-            labels.append(float(row["target"]))
-
-    if not rows:
-        raise ValueError("CSV dataset is empty.")
-
-    x = torch.tensor(rows, dtype=torch.float32)
-    y = torch.tensor(labels, dtype=torch.float32).unsqueeze(1)
-    return x, y
-
-
-def split_dataset(x: torch.Tensor, y: torch.Tensor, seed: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-    generator = torch.Generator().manual_seed(seed)
-    perm = torch.randperm(x.shape[0], generator=generator)
-    split = int(x.shape[0] * 0.8)
-    train_idx, val_idx = perm[:split], perm[split:]
-    return x[train_idx], y[train_idx], x[val_idx], y[val_idx]
+    x_train = torch.tensor(train_df[FEATURE_NAMES].values, dtype=torch.float32)
+    y_train = torch.tensor(train_df[TARGET_NAME].values, dtype=torch.float32).unsqueeze(1)
+    x_val = torch.tensor(val_df[FEATURE_NAMES].values, dtype=torch.float32)
+    y_val = torch.tensor(val_df[TARGET_NAME].values, dtype=torch.float32).unsqueeze(1)
+    return x_train, y_train, x_val, y_val
 
 
 def train_model(
@@ -137,27 +123,42 @@ def train_model(
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Train wildfire PyTorch model.")
-    parser.add_argument("--input-csv", type=Path, default=None)
-    parser.add_argument("--output-path", type=Path, default=Path("src/wildfire/model/wildfire_model.pt"))
-    parser.add_argument("--epochs", type=int, default=200)
-    parser.add_argument("--batch-size", type=int, default=128)
+    parser = argparse.ArgumentParser(description="Train wildfire PyTorch model from real data.")
+    parser.add_argument(
+        "--input-csv",
+        type=Path,
+        default=Path("src/wildfire/data/raw/forestfires.csv"),
+        help="Path to the downloaded raw wildfire CSV.",
+    )
+    parser.add_argument(
+        "--processed-csv",
+        type=Path,
+        default=Path("src/wildfire/data/processed/wildfire_training.csv"),
+        help="Where to write processed training rows for inspection.",
+    )
+    parser.add_argument(
+        "--output-path",
+        type=Path,
+        default=Path("src/wildfire/model/wildfire_model.pt"),
+        help="Where to store trained model artifact.",
+    )
+    parser.add_argument("--epochs", type=int, default=300)
+    parser.add_argument("--batch-size", type=int, default=64)
     parser.add_argument("--learning-rate", type=float, default=1e-3)
-    parser.add_argument("--samples", type=int, default=4000)
     parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--model-version", type=str, default="0.1.0")
+    parser.add_argument("--model-version", type=str, default="0.2.0")
     return parser.parse_args()
 
 
 def main() -> None:
     args = parse_args()
+    raw_df = load_raw_dataset(args.input_csv)
+    training_df = prepare_training_dataframe(raw_df)
 
-    if args.input_csv is not None:
-        x, y = load_csv_dataset(args.input_csv)
-    else:
-        x, y = generate_synthetic_dataset(samples=args.samples, seed=args.seed)
+    args.processed_csv.parent.mkdir(parents=True, exist_ok=True)
+    training_df.to_csv(args.processed_csv, index=False)
 
-    x_train, y_train, x_val, y_val = split_dataset(x, y, seed=args.seed)
+    x_train, y_train, x_val, y_val = split_dataset(training_df, seed=args.seed)
     model, feature_mean, feature_std, val_accuracy = train_model(
         x_train=x_train,
         y_train=y_train,
@@ -178,10 +179,12 @@ def main() -> None:
             "model_version": args.model_version,
             "feature_names": FEATURE_NAMES,
             "val_accuracy": val_accuracy,
+            "dataset_rows": int(len(training_df)),
         },
         args.output_path,
     )
 
+    print(f"Saved processed data to: {args.processed_csv}")
     print(f"Saved model to: {args.output_path}")
     print(f"Validation accuracy: {val_accuracy:.4f}")
 
