0c9513e03cdc4d607520f0110ca74b7f9ad6901d
Tony Ivchenko <toxa.ivchenko@gmail.com>
2024-01-05 15:08:17 -0800

    merge duplicate lidc xml corrections in ctscan prep

--- src/ctscan/tests/nodules/test_download_data.py
+++ src/ctscan/tests/nodules/test_download_data.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 import json
 from pathlib import Path
+import zipfile
 
 import numpy as np
 
 from src.ctscan.models.nodules import PATCH_SHAPE
 from src.ctscan.scripts.nodules.download_data import (
     build_smoke_training_dataset,
+    collect_real_annotation_manifest,
     fetch_series_uid,
     parse_lidc_xml_bytes,
     write_dataset_manifest,
@@ -88,3 +90,52 @@ def test_parse_lidc_xml_bytes_extracts_series_and_malignancy():
     assert parsed["annotations"][0]["sop_uids"] == ["sop-1", "sop-1"]
     assert parsed["non_nodules"][0]["sop_uid"] == "sop-2"
     assert parsed["non_nodules"][0]["x_coord"] == 40
+
+
+def test_collect_real_annotation_manifest_merges_duplicate_series(tmp_path: Path):
+    xml_payload = b"""
+    <LidcReadMessage xmlns="http://www.nih.gov">
+      <ResponseHeader>
+        <SeriesInstanceUid>1.2.3</SeriesInstanceUid>
+        <StudyInstanceUID>4.5.6</StudyInstanceUID>
+      </ResponseHeader>
+      <readingSession>
+        <servicingRadiologistID>reader-a</servicingRadiologistID>
+        <unblindedReadNodule>
+          <noduleID>n1</noduleID>
+          <roi>
+            <imageSOP_UID>sop-1</imageSOP_UID>
+            <inclusion>TRUE</inclusion>
+            <edgeMap><xCoord>20</xCoord><yCoord>30</yCoord></edgeMap>
+          </roi>
+          <characteristics><malignancy>4</malignancy></characteristics>
+        </unblindedReadNodule>
+      </readingSession>
+    </LidcReadMessage>
+    """
+    xml_payload_2 = b"""
+    <LidcReadMessage xmlns="http://www.nih.gov">
+      <ResponseHeader>
+        <SeriesInstanceUid>1.2.3</SeriesInstanceUid>
+        <StudyInstanceUID>4.5.6</StudyInstanceUID>
+      </ResponseHeader>
+      <readingSession>
+        <servicingRadiologistID>reader-b</servicingRadiologistID>
+        <nonNodule>
+          <nonNoduleID>nn-1</nonNoduleID>
+          <imageSOP_UID>sop-2</imageSOP_UID>
+          <locus><xCoord>40</xCoord><yCoord>50</yCoord></locus>
+        </nonNodule>
+      </readingSession>
+    </LidcReadMessage>
+    """
+    xml_path = tmp_path / "lidc.xml.zip"
+    with zipfile.ZipFile(xml_path, "w") as archive:
+        archive.writestr("a.xml", xml_payload)
+        archive.writestr("b.xml", xml_payload_2)
+
+    manifest = collect_real_annotation_manifest(xml_path)
+    assert len(manifest) == 1
+    assert len(manifest[0]["annotations"]) == 1
+    assert len(manifest[0]["non_nodules"]) == 1
+    assert manifest[0]["xml_names"] == ["a.xml", "b.xml"]
